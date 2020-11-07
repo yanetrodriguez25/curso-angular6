bffee3bd1b1ee2f85c9e47eef996e8dcc1d6a019
“Yanet <yanet.rodriguez@mercadolibre.com>
2020-11-07 18:16:30 -0300

    whishlist

--- angular-whislist/src/app/app.module.ts
+++ angular-whislist/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SaludadorComponent } from './saludador/saludador.component';
+import { DestinoViajeComponent } from './destino-viaje/destino-viaje.component';
+import { ListaDestinosComponent } from './lista-destinos/lista-destinos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SaludadorComponent
+    DestinoViajeComponent,
+    ListaDestinosComponent
   ],
   imports: [
     BrowserModule,
---
+++ angular-whislist/src/app/lista-destinos/lista-destinos.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { DestinoViaje } from './../Models/destino-viaje.model';
+
+@Component({
+  selector: 'app-lista-destinos',
+  templateUrl: './lista-destinos.component.html',
+  styleUrls: ['./lista-destinos.component.css']
+})
+export class ListaDestinosComponent implements OnInit {
+  destinos: DestinoViaje[];
+  
+  constructor() {
+  	this.destinos = [];
+  }
+
+  ngOnInit(): void {
+  }
+
+  guardar(nombre:string, url:string):boolean {
+  	this.destinos.push(new DestinoViaje(nombre,url));
+  	return false;
+  }
+
+}
