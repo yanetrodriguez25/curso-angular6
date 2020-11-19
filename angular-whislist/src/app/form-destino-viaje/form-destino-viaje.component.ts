d86f80103818a7ff0570a4bd26f10f1d2e1a0fd2
“Yanet <yanet.rodriguez@mercadolibre.com>
2020-11-18 23:51:24 -0300

    validators

--- angular-whislist/src/app/form-destino-viaje/form-destino-viaje.component.ts
+++ angular-whislist/src/app/form-destino-viaje/form-destino-viaje.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Form, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 import { DestinoViaje } from '../Models/destino-viaje.model';
 
 @Component({
@@ -10,10 +10,15 @@ import { DestinoViaje } from '../Models/destino-viaje.model';
 export class FormDestinoViajeComponent implements OnInit {
   @Output() onItemAdded: EventEmitter<DestinoViaje>;
   fg: FormGroup;
+  minLongitud = 3;
   constructor(fb: FormBuilder) { 
     this.onItemAdded = new EventEmitter();
     this.fg = fb.group({
-      nombre:['', Validators.required],
+      nombre:['',  Validators.compose([
+        Validators.required,
+        this.nombreValidator,
+        this.nombreValidatorParametrizable(this.minLongitud)
+      ])],
       url:['']
     });
 
@@ -31,4 +36,22 @@ export class FormDestinoViajeComponent implements OnInit {
     return false;
   }
 
+  nombreValidator(control :FormControl): {[s :string]: boolean} {
+    const l = control.value.toString().trim().length;
+    if( l > 0 && l < 5)
+      return {invalidNombre: true };
+     
+    return null;  
+  }
+
+  nombreValidatorParametrizable(minLong: number): ValidatorFn {
+    return (control :FormControl): {[s :string]: boolean} | null => {
+      const l = control.value.toString().trim().length;
+
+      if( l > 0 && l < minLong)
+        return {minLongNombre: true };
+
+      return null;
+    }
+  }
 }
